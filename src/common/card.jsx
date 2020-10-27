e46be7edfafe326ad07768c4eba6b165d31f107f
AssanSav <assane.savadogo81@gmail.com>
2020-10-26 23:23:22 -0400

    card component to share the users list and matches list pages

---
+++ src/common/card.jsx
@@ -0,0 +1,35 @@
+
+import React from 'react';
+import { Link } from "react-router-dom";
+
+
+export const card = (user) => {
+
+  return (
+    <div className="profile" key={`${user.id}`}>
+      <div className="image-align">
+        <Link to={`/profile/${user.id}`}>
+          <img src={user.image} alt="" id="avatar_img" />
+        </Link>
+        <p>
+          <strong>
+            <Link to={`/match-profile/${user.id}`}>
+              {user.username}
+            </Link>
+          </strong>
+        </p>
+        <p>
+          <strong>Age:</strong>
+          {user.age}
+        </p>
+        <p>
+          <strong>Orientation:</strong>
+          {user.orientation}
+        </p>
+        <Link to={`/profile/${user.id}`}>
+          <button className="btn btn-success">View Profile</button>
+        </Link>
+      </div>
+    </div>
+  )
+}
