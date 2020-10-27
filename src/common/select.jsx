b60c92c14354a679d496a006487dea5039952600
AssanSav <assane.savadogo81@gmail.com>
2020-10-27 01:16:10 -0400

    Reusable select options component

---
+++ src/common/select.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Select = ({ name, label, options, error, ...rest }) => {
+  return (
+    <>
+      <div className="form-group">
+        <select name={name} id={name} {...rest} className="form-control">
+          <option value="" selected hidden>{label}</option>
+          {options.map((option, index) => (
+            <option key={index} value={option} >
+              {option}
+            </option>
+          ))}
+        </select>
+        {error && <div className="alert alert-danger">{error}</div>}
+      </div>
+    </>
+  );
+};
+
+export default Select;
